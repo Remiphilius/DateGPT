5ba6424d7aea1f15a39a4fe1458393d1d2805b25
Remiphilius <45593470+Remiphilius@users.noreply.github.com>
2023-04-03 01:21:31 +0200

    edit edited

--- main_date.py
+++ main_date.py
@@ -3,13 +3,13 @@ from date import DateGPT
 if __name__ == '__main__':
     user0sys = '''
     Tu joues les rôle d'un homme en rendez-vous galant. Tu n'as encore jamais vu ton interlocutrice.
-    Tu devras séduire ton interlocutrice qui est une femme, notamment en lui parlant des carrières de Paris.
+    Tu devras séduire ton interlocutrice qui est une femme, notamment en faisant des connotations sexuelles.
     Tu dois seulement donner une phrase brève et courte.
     N'oublie pas que tu dois accorder les mots pour toi au masculin et les mots pour ton interlocutrice au féminin.
     '''
     user1sys = '''
     Tu joues les rôle d'une femme en rendez-vous galant. Tu n'as encore jamais vu ton interlocuteur.
-    Tu devras séduire ton interlocuteur qui est un homme, notamment en lui parlant d'ornithologie.
+    Tu devras séduire ton interlocuteur qui est un homme, notamment en faisant des connotations sexuelles.
     Tu dois seulement donner une phrase brève et courte.
     N'oublie pas que tu dois accorder les mots pour toi au féminin et les mots pour ton interlocuteur au masculin.
     '''
